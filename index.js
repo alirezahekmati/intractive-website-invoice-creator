af62db31ac832bfafe712f3a9fb338e75a149117
alireza <alirezahekmati80@gmail.com>
2022-02-13 17:45:35 -0800

    using remove  method

--- index.js
+++ index.js
@@ -1,115 +1,36 @@
-const taskLeftEl  = document.querySelector(".task-factor .left ")
-const taskRightE  = document.querySelector(".task-factor .right ")
-const span = document.querySelector("span")
-let isDispalyedaddPul=false
-let isDispalyedaddWash=false
-let isDispalyedaddMow=false
-let totalPrice=0
-let a =document.querySelector(".p")
-let b =document.querySelector(".w")
-let c =document.querySelector(".m")
-let aid =document.querySelector("#p")
-let bid =document.querySelector("#w")
-let cid =document.querySelector("#m")
 
-function addWash(){
-    
-    if(!isDispalyedaddWash){
-        taskLeftEl.innerHTML += ` 
-                    <div  id="w" class="list-margin-top-10">
-                    <span class="w list-margin-top-10">Wash Car</span>
-                    <span onclick="remover_w()" class="btn_remove">remove</span>
-                    </div>
-                                `
-        taskRightE.innerHTML += ` <div class="w list-margin-top-10">$10</div>`
-        
-        totalPrice +=10
-        span.innerHTML = `$${totalPrice}`
+const leftEl= document.querySelector(".left_1")
+const rightEl= document.querySelector(".right_1")
+const leftEmptyEl = document.querySelector(".left_empty")
+const rightEmptyEl = document.querySelector(".right_empty")
+
+let isDisplayedAddWash = false
+let isDisplayedAddMow = false
+let isDisplayedAddPul = false
+
+
+
+function addWash() {
+    if(!isDisplayedAddWash){
+        leftEmptyEl.innerHTML += `<span id="a1">Wash Car</span> <span id="a3" onclick="remove_a1()">Remove</span>`
+        rightEmptyEl.innerHTML += `<span id="a2">$10</span>`
     }
-    b =document.querySelector(".w")
-    bid =document.querySelector("#w")
-   
-    isDispalyedaddWash =true
-    
-} 
-function addMow(){
-    
-    if(!isDispalyedaddMow){
-        taskLeftEl.innerHTML += ` 
-                    <div" id="m" class="list-margin-top-10">
-                    <span class="m list-margin-top-10">Mow Lawn</span>
-                    <span  onclick="remover_m()" class="btn_remove" >remove</span>
-                    </div>
-                                `
-        taskRightE.innerHTML += ` <div class="m list-margin-top-10">$20</div>`
-        
-        totalPrice +=20
-        span.innerHTML = `$${totalPrice}`
-    }
-    c =document.querySelector(".m")
-    cid =document.querySelector("#m")
-    isDispalyedaddMow =true
-        
+    isDisplayedAddWash= true
 }
-function addPul(){
+function remove_a1(){
+    document.getElementById("a1").remove()
+    document.getElementById("a2").remove()
+    document.getElementById("a3").remove()
     
-    if(!isDispalyedaddPul){
-    taskLeftEl.innerHTML += ` 
-                <div id="p"  class="list-margin-top-10">
-                <span class="p list-margin-top-10">Pull Weeds</span>
-                <span  onclick="remover_p()"  class="btn_remove">remove</span>
-                </div>
-                            `
-    taskRightE.innerHTML += ` <div class="p list-margin-top-10">$30</div>`
-   
-    totalPrice +=30 
-    span.innerHTML = `$${totalPrice}`
-    
-    } 
-    a =document.querySelector("#p")
-    aid =document.querySelector(".p")
-    isDispalyedaddPul=true 
-}
-function sendOrder(){
-    taskLeftEl.innerHTML = ` <div >task</div>`
-    taskRightE.innerHTML = ` <div >total</div>`
-    isDispalyedaddPul=false
-    isDispalyedaddWash=false
-    isDispalyedaddMow=false
-    totalPrice =0
-    span.innerHTML = `$${totalPrice}`
+    isDisplayedAddWash= false
 
 }
-
-function remover_p(){
-    console.log(a.innerHTML)
-    a.innerHTML=""
-    aid.innerHTML=""
-    isDispalyedaddPul=false
+// function addMow() {
     
+//     isDisplayedAddMow= true
+// }
+// function addPul() {
     
-}
-function remover_w(){
-    console.log(b.innerHTML)
-    b.innerHTML=""
-    bid.innerHTML=""
-    isDispalyedaddWash=false
-    
-    
-}
-function remover_m(){
-    console.log(c.innerHTML)
-    c.innerHTML=""
-    cid.innerHTML=""
-    isDispalyedaddMow=false
-    
-    
-}
-
-
-    
-    
-
-
-
+//     isDisplayedAddPul= true
+// }
 
