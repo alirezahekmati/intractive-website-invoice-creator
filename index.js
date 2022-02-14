f80c6df27d47a6d66eae5587da325f6093f0b678
alireza <alirezahekmati80@gmail.com>
2022-02-13 18:17:51 -0800

    finished perfecty

--- index.js
+++ index.js
@@ -3,34 +3,76 @@ const leftEl= document.querySelector(".left_1")
 const rightEl= document.querySelector(".right_1")
 const leftEmptyEl = document.querySelector(".left_empty")
 const rightEmptyEl = document.querySelector(".right_empty")
+const priceEL =document.getElementById("total_price")
 
 let isDisplayedAddWash = false
 let isDisplayedAddMow = false
 let isDisplayedAddPul = false
 
-
+let totalPrice  = 0
 
 function addWash() {
     if(!isDisplayedAddWash){
-        leftEmptyEl.innerHTML += `<span id="a1">Wash Car</span> <span id="a3" onclick="remove_a1()">Remove</span>`
-        rightEmptyEl.innerHTML += `<span id="a2">$10</span>`
+        leftEmptyEl.innerHTML += `<div id="a1">Wash Car <span id="a3" onclick="remove_a1()">Remove</span></div>`
+        rightEmptyEl.innerHTML += `<div id="a2">$10</div>`
     }
+    totalPrice += 10
     isDisplayedAddWash= true
+    priceEL.innerHTML="$"+totalPrice
 }
 function remove_a1(){
     document.getElementById("a1").remove()
     document.getElementById("a2").remove()
-    document.getElementById("a3").remove()
-    
+    // document.getElementById("a3").remove()
+    totalPrice -= 10
+    priceEL.innerHTML="$"+totalPrice
     isDisplayedAddWash= false
 
 }
-// function addMow() {
-    
-//     isDisplayedAddMow= true
-// }
-// function addPul() {
-    
-//     isDisplayedAddPul= true
-// }
+///////////////////////////////////////////////////////////
+function addMow() {
+    if(!isDisplayedAddMow){
+        leftEmptyEl.innerHTML += `<div id="b1">Mow Lawn <span id="b3" onclick="remove_b1()">Remove</span></div>`
+        rightEmptyEl.innerHTML += `<div id="b2">$20</div>`
+    }
+    totalPrice += 20
+    isDisplayedAddMow= true
+    priceEL.innerHTML="$"+totalPrice
+}
+function remove_b1(){
+    document.getElementById("b1").remove()
+    document.getElementById("b2").remove()
+    // document.getElementById("b3").remove()
+    totalPrice -= 20
+    priceEL.innerHTML="$"+totalPrice
+    isDisplayedAddMow= false
+}
+///////////////////////////////////////////////////////////////
+function addPul() {
+    if(!isDisplayedAddPul){
+        leftEmptyEl.innerHTML += `<div id="c1">Pull Weeds <span id="c3" onclick="remove_c1()">Remove</span></div>`
+        rightEmptyEl.innerHTML += `<div id="c2">$30</div>`
+    }
+    totalPrice += 30
+    isDisplayedAddPul= true
+    priceEL.innerHTML="$"+totalPrice
+}
+function remove_c1(){
+    document.getElementById("c1").remove()
+    document.getElementById("c2").remove()
+    // document.getElementById("c3").remove()
+    totalPrice -= 30
+    priceEL.innerHTML="$"+totalPrice
+    isDisplayedAddPul= false
+
+}
+function sendOrder(){
+    leftEmptyEl.innerHTML = ``
+    rightEmptyEl.innerHTML = ``
+    totalPrice = 0
+    priceEL.innerHTML="$"+totalPrice
+    isDisplayedAddWash = false
+    isDisplayedAddMow = false
+    isDisplayedAddPul = false
+}
 
